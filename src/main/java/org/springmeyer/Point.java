2947f78299d33c80a50aeb3f94cd3131399568fb
springmeyer <dane.springmeyer@gmail.com>
2024-06-26 12:11:27 -0700

    integer point coordinates

--- src/main/java/org/springmeyer/Point.java
+++ src/main/java/org/springmeyer/Point.java
@@ -1,15 +1,19 @@
 package org.springmeyer;
 
 public class Point {
-  public double x;
-  public double y;
+  public int x;
+  public int y;
 
-  public Point(double x, double y) {
+  public Point(int x, int y) {
     this.x = x;
     this.y = y;
   }
 
   public Point clone() {
     return new Point(this.x, this.y);
+  }
+
+  public String toString() {
+    return "{x:" + this.x + ", y:" + this.y + "}";
   }
 }
